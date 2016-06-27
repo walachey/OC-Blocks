fe81c7a5e5d2d458083ac4812c1b53ddbd69cf33
Thow <TheThow@users.noreply.github.com>
2016-06-27 16:17:04 +0200

    Changed wood tiles to 12x12

--- Blocks.ocd/Tiles.ocd/Tile_WallWood.ocd/Script.c
+++ Blocks.ocd/Tiles.ocd/Tile_WallWood.ocd/Script.c
@@ -6,6 +6,12 @@ local Name = "$Name$";
 local Description = "$Description$";
 local Collectible = 1;
 
+local build_grid_x = 12;
+local build_grid_y = 12;
+
+local tile_size_x = 12;
+local tile_size_y = 12;
+
 protected func Hit(x, y)
 {
 	StonyObjectHit(x,y);
